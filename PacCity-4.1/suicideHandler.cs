f53ee8b1db23687d7965fa463baccda4539eb27d
Pacnet Netty <bhegde8@gmail.com>
2014-07-13 12:48:36 -0400

    suicideHandler.cs package
    This code was meant to be packaged but it slipped through and overwrote the suicide server command. So it's fixed. Not that this project is active but I just noticed this, so oh well.

--- PacCity-4.1/suicideHandler.cs
+++ PacCity-4.1/suicideHandler.cs
@@ -1,19 +1,23 @@
-function serverCmdSuicide(%client)
+package pacCitySuicideHandler
 {
-    if(%client.jailed == 1)  
-    {
-        messageClient(%client, '', '\c4You cannot suicide in jail.');
-        return;
-    }
-    else
-    {
-        %client.instantRespawn();
-        if(%client.forceSleep == true)
-        {
-	        %client.isAsleep = true;
-	        %client.forceSleep = true;
-	        %client.player.kill("suicide");
-	        messageClient(%client, '', '\c4Resting.');
-        }
-    }
-}
+	function serverCmdSuicide(%client)
+	{
+	    if(%client.jailed == 1)  
+	    {
+	        messageClient(%client, '', '\c4You cannot suicide in jail.');
+	        return;
+	    }
+	    else
+	    {
+	        %client.instantRespawn();
+	        if(%client.forceSleep == true)
+	        {
+		        %client.isAsleep = true;
+		        %client.forceSleep = true;
+		        %client.player.kill("suicide");
+		        messageClient(%client, '', '\c4Resting.');
+	        }
+	    }
+	}
+};
+activatePackage(pacCitySuicideHandler);
